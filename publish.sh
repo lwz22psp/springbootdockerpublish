9fbac1291ffbfd608fec9be38fd34085fe55663a
Dark <lwz22psp@hotmail.com>
2019-03-22 09:21:07 +0800

    add echo

--- publish.sh
+++ publish.sh
@@ -2,13 +2,30 @@
 #切换路径
 cd /root/github/springbootdemo/
 #拉取最新代码
+echo "git pull start"
+
 git pull
+
+echo "git pull finish"
+
 #停用旧容器
+
+echo "stop old docker"
+
 docker stop springbootdocker
+
+echo "remove old docker"
+
 docker rm springbootdocker
+
+echo "remove old image"
 #删除旧镜像
 docker image rm springboot/springbootdocker
+
+echo "build new image"
 #编译镜像
 mvn package docker:build
+
+echo "run new docker"
 #挂载镜像到新容器中
 docker run -d -p 8080:8080 --name springbootdocker springboot/springbootdocker
